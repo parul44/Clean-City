d013a8c2a282bde1ee1a6899800e4974cba8bc5d
blitz450 <saurabh.chauhan450@gmail.com>
2019-04-18 02:45:43 +0530

    Dummy Routes

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
 // Main server file
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
-
-const userRoutes = require("./routes/userRoutes");
-const adminRoutes = require("./routes/adminRoutes");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "client")));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Routes
 
-// User route
-app.use("/user", userRoutes);
+// User routes
+app.use('/user', userRoutes);
 
-// Admin route
-app.use("/admin", adminRoutes);
+// Admin routes
+app.use('/admin', adminRoutes);
 
-app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/client/index.html");
+//html routes
+app.get('/about', (req, res, next) => {
+  res.sendFile(__dirname + '/client/about.html');
+});
+
+app.get('/form', (req, res, next) => {
+  res.sendFile(__dirname + '/client/form.html');
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 app.listen(PORT, () => {

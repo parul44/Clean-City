5a20e539b0515030b81be3b30427e1fa68545f57
blitz450 <saurabh.chauhan450@gmail.com>
2019-06-10 14:24:24 +0530

    Reports filtering

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -39,9 +39,9 @@ const postSubmitData = async (req, res) => {
     };
 
     const response = await fetch(
-      `http://apis.mapmyindia.com/advancedmaps/v1/o223g5iid9kb1pimi74ltebthdi64q3r/rev_geocode?lat=${
-        req.body.latitude
-      }&lng=${req.body.longitude}`
+      `http://apis.mapmyindia.com/advancedmaps/v1/${
+        process.env.API_KEY
+      }/rev_geocode?lat=${req.body.latitude}&lng=${req.body.longitude}`
     );
 
     const data = await response.json();
@@ -51,7 +51,7 @@ const postSubmitData = async (req, res) => {
     await report.save();
     res.status(201).send(`Report added to DB! with id ${report._id}`);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send({ error: e.errmsg });
   }
 };
 
@@ -84,7 +84,9 @@ const getReports = async (req, res) => {
   }
 
   if (req.query.pincode) {
-    if (req.query.pincode.length) match.pincode = Number(req.query.pincode);
+    if (req.query.pincode.length) {
+      match['results.pincode'] = req.query.pincode;
+    }
   }
 
   if (req.query.sortBy) {
@@ -107,14 +109,14 @@ const getReports = async (req, res) => {
     res.status(400).send(e);
   }
 };
-const getcount = async (req,res) =>{
-  try{
-    let count = await Report.countDocuments({},function(err,c){
-      if(err){
+const getCount = async (req, res) => {
+  try {
+    let count = await Report.countDocuments({}, function(err, c) {
+      if (err) {
         console.log(err);
       }
     });
-    res.sendStatus(200).send(count);
+    res.status(200).send({ count });
   } catch (e) {
     res.status(404).send(e);
   }
@@ -155,5 +157,5 @@ module.exports = {
   getReports,
   getReportsID,
   getImage,
-  getcount
+  getCount
 };

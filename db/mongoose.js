e4e1c1766bceb6e84e4323363ac934877d3c3d14
blitz450 <saurabh.chauhan450@gmail.com>
2019-07-24 02:43:16 +0530

    FE File validation/ error handling

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(console.log('Connected to DB'))
-  .catch(e => console.log(e));
+	.connect(process.env.DB_URI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
+	})
+	.then(console.log('Connecting to DB'))
+	.catch(e => console.log(e))
